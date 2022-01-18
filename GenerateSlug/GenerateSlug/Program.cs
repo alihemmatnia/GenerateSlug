6914bdc3d80cd730d1a991efd431d771eb8f4b94
Ali2004h <ali2004h.linux@gmail.com>
2022-01-18 14:29:43 +0330

    First Commit

---
+++ GenerateSlug/GenerateSlug/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenerateSlug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "what is the csharp programming language";
+            Console.WriteLine(test.toSlug());
+      }
+    }
+}
